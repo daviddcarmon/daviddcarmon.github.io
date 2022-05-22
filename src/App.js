14dd6ac183aab772ef6e9e677d05cca916de86db
daviddcarmon <daviddcarmona@gmail.com>
2022-05-22 17:22:29 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/pages/about";
 import Projects from "./components/pages/projects";
-import Body from './components/pages/body'
+import Body from "./components/pages/body";
 import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <div
-      className="App"
-      
-    >
+    <div className="App">
       <NavBar />
-      <Router>
+      <Router
+        basename={optionalString}
+        getUserConfirmation={optionalFunc}
+        hashType={optionalString}
+      >
         <Routes>
           <Route path="/" element={<Body />} />
           <Route path="/#/contact" element={<About />} />
